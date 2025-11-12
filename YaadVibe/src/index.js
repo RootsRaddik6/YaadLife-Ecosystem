d59892d3e3d22f179893f1e5729865a32879d207
RootsRaddik6 <korjamltd@gmail.com>
2025-11-12 10:37:29 -0500

    Update index.js

--- YaadVibe/src/index.js
+++ YaadVibe/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+// Telegram WebApp init
+if (window.Telegram?.WebApp) {
+  window.Telegram.WebApp.ready();
+  window.Telegram.WebApp.expand();
+}
+
+// Testnet config for TWA
+const manifestUrl = window.Telegram?.WebApp ? '/testnet-config.json' : 'https://ton.org/tonconnect-manifest.json';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TonConnectUIProvider manifestUrl="https://raw.githubusercontent.com/RootsRaddik6/YaadLife-Ecosystem/main/YaadVibe/public/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
   </TonConnectUIProvider>
 );
